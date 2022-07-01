8271e1ea7ec74a46fa90a1dc7ba2455320e90d5c
Amir mahdi Rezae Tavana <amirredfox117@gmail.com>
2022-07-01 03:49:46 -0700

    Set Screen Attribute

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from turtle import Screen, Turtle, width
+from turtle import Screen, Turtle
 
 
 screen = Screen()
 screen.setup(width=800, height=600)
-
-
+screen.bgcolor("black")
+screen.title("Pong")
 screen.exitonclick()
