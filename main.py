0aed556cfa4f67365ae01900cc17fcafcdf4b8d1
Amir mahdi Rezae Tavana <amirredfox117@gmail.com>
2022-07-01 04:26:07 -0700

    Add Game loop

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from turtle import Screen
 from Paddle import Paddle
-
-
+import time
+# Initial SCcreen
 screen = Screen()
+screen.tracer(0)
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 
-paddle = Paddle()
 screen.listen()
+paddle = Paddle()
 
 screen.onkey(paddle.Up, "Up")
 screen.onkey(paddle.Down, "Down")
+
+
+Game_Is_on = True
+
+while Game_Is_on:
+    screen.update()
+    time.sleep(0.1)
+
 
 screen.exitonclick()
